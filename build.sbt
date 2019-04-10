3d3af8a5ac7e62723bb593607fef2e9f93cf310d
Vinothkumar Chandran <vchandran@tekion.com>
2019-04-10 19:09:58 +0530

    Spark word count program initial commit.

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "Spark Learning"
+
+version := "1.0"
+
+scalaVersion := "2.12.8"
+
+sbtVersion := "1.2.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.4.1"
+)
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,15 @@
+import org.apache.spark.internal.config.SparkConfigProvider
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val sc = SparkContext.getOrCreate(new SparkConf().setAppName("Word Count").setMaster("local"));
+    
+    val words = sc.textFile("story.txt").flatMap(line => line.split(" "))
+    val wordCountIndex = words.map((_, 1)).reduceByKey(_ + _)
+
+    wordCountIndex.collect().foreach(println)
+  }
+}
