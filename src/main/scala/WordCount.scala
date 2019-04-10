f2aaa19f00a380a20440c4f5f31d67c293013d3d
Vinothkumar Chandran <chandranvinoth08@gmail.com>
2019-04-10 22:36:31 +0530

    Utilizing scala's brevity.

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -7,7 +7,7 @@ object WordCount {
   def main(args: Array[String]): Unit = {
     val sc = SparkContext.getOrCreate(new SparkConf().setAppName("Word Count").setMaster("local"));
     
-    val words = sc.textFile("story.txt").flatMap(line => line.split(" "))
+    val words = sc.textFile("story.txt").flatMap(_.split(" "))
     val wordCountIndex = words.map((_, 1)).reduceByKey(_ + _)
 
     wordCountIndex.collect().foreach(println)
